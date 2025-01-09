2b7981892cc3f0ab1833880f267559c340964816
abufaisal179 <abufaishal179@gmail.com>
2025-01-09 22:45:39 +0530

    Create 3dOperationInArray.cpp

---
+++ 2D_Arrays/3dOperationInArray.cpp
@@ -0,0 +1,40 @@
+// Hey guys , its Faisal's Program : TITLE >> {}
+#include <iostream>
+using namespace std;
+
+int main(void) {
+
+    int arr[2][2][2];
+
+    
+    for( int i=0; i<2; i++ )  // 1
+    {
+        for( int j=0; j<2; j++ ) // 2
+        {
+            for( int k=0; k<2; k++ ) // 4
+            {
+                for( int l=0; l<2; l++ )
+                {
+                    for( int m=0; m<2; m++ )
+                {
+                   for( int n=0; n<2; n++ )
+                {
+                   for( int o=0; o<2; o++ )
+                {
+                  for( int s=0; s<45; s++ )
+                  {
+                     cout << i << " \t ";
+                  }
+                }
+                }
+                }
+                   
+                }
+            }
+            cout << endl;
+        }
+    }
+ 
+
+  return 0;
+}
