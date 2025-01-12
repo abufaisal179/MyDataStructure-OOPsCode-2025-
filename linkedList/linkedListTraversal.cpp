90e01e77c52ceb1f166c85e9d73b3e70a6f72c65
abufaisal179 <abufaishal179@gmail.com>
2025-01-12 20:09:35 +0530

    Update linkedListTraversal.cpp

--- linkedList/linkedListTraversal.cpp
+++ linkedList/linkedListTraversal.cpp
@@ -28,29 +28,45 @@ void printAllData(Node *&head)
         temp = temp->next;
     }
 }
-void insertAtMiddle( int data , Node *head , int n )
+
+void insertAtMiddle(int data, Node *&head, int position)
 {
-    
+    if (position == 1)
+    {
+        insertAtHead(data, head);
+        return;
+    }
+    int count = 1;
+    Node *temp = head;
+    while (count < position - 1 && temp != NULL)
+    {
+        temp = temp->next;
+        count++;
+    }
+    Node *nodeToInsert = new Node(data);
+    nodeToInsert->next = temp->next;
+    temp->next = nodeToInsert;
 }
+
 void insertAtTail(int data, Node *&tail)
 {
     Node *temp = new Node(data);
     tail->next = temp;
-    temp->next = NULL;
-    tail = temp;
+    tail = tail->next;
 }
 int main(void)
 {
 
-    Node *node = new Node(4);
-    Node *tail = node;
+    Node *head = new Node(4);
+    Node *tail = head;
+    insertAtHead(8, head);
+    insertAtTail(6, tail);
+    insertAtTail(5, tail);
+    insertAtTail(3, tail);
+    insertAtTail(89, tail);
+    insertAtMiddle(14 , head , 3);
 
-    insertAtTail(6, node);
-    insertAtTail(5, node);
-    insertAtTail(3, node);
-    insertAtTail(89, node);
-
-    printAllData(tail);
+    printAllData(head);
 
     return 0;
 }
