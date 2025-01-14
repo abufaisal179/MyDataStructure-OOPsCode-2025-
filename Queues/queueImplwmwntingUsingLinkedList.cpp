fae2d7529e46c184a405f553b315bac33715fdd9
abufaisal179 <abufaishal179@gmail.com>
2025-01-14 13:47:35 +0530

    Create queueImplwmwntingUsingLinkedList.cpp

---
+++ Queues/queueImplwmwntingUsingLinkedList.cpp
@@ -0,0 +1,18 @@
+// Hey guys , its Faisal's Program : TITLE >> {}
+#include <iostream>
+using namespace std;
+class Queue
+{
+   public:
+   int data;
+   Queue* next;
+   Queue ( int data )
+   {
+       next = NULL;
+       this-> data = data;
+   }
+};
+int main(void) {
+
+  return 0;
+}
