757ba6cacab15fc7c73165acecf71d4da25b45ca
abufaisal179 <abufaishal179@gmail.com>
2025-01-15 08:13:23 +0530

    Update circularLinkedListTraversal.cpp

--- linkedList/circularLinkedListTraversal.cpp
+++ linkedList/circularLinkedListTraversal.cpp
@@ -1,50 +1,49 @@
-// Hey guys , its Faisal's Program : TITLE >> {}
-#include <iostream>
-using namespace std;
-class Node
-{
-public:
-    int data;
-    Node *next;
+// // Hey guys , its Faisal's Program : TITLE >> {}
+// #include <iostream>
+// using namespace std;
+// class Node
+// {
+// public:
+//     int data;
+//     Node *next;
 
-    Node(int data)
-    {
-        this->data = data;
-        next = NULL;
-    }
-};
-void insertNode(int data, Node *&tail, int ele)
-{
-    if (tail == NULL)
-    {
-        Node *newNode = new Node(data);
-        tail = newNode;
-        newNode->next = newNode;
-    }
-    else
-    {
-        Node *curr = tail;
+//     Node(int data)
+//     {
+//         this->data = data;
+//         next = NULL;
+//     }
+// };
+// void insertNode(int data, Node *&tail, int ele)
+// {
+//     if (tail == NULL)
+//     {
+//         Node *newNode = new Node(data);
+//         tail = newNode;
+//         newNode->next = newNode;
+//     }
+//     else
+//     {
+//         Node *curr = tail;
 
-        while ( curr -> data != ele )
-        {
-            curr = curr -> next;
-        }
-        Node *temp = new Node ( data );
-        temp -> next = curr -> next;
-        curr -> next = temp;
+//         while (curr->data != ele)
+//         {
+//             curr = curr->next;
+//         }
+//         Node *temp = new Node(data);
+//         temp->next = curr->next;
+//         curr->next = temp;
+//     }
+// }
+// void printNode(Node *&tail)
+// {
+//     Node *temp = tail;
+//     while ()
+// }
+// int main(void)
+// {
 
-    }
-}
-void printNode ( Node *&tail ) 
-{
-    Node *temp = tail;
-    while( )
-}
-int main(void)
-{
+//     Node *tail = new Node(4);
 
-    Node *tail = new Node(4);
-
-    insertNode( 9 , tail , 5);
-    return 0;
-}
+//     insertNode(9, tail, 5);
+//     return 0;
+// }
