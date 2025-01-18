bd867965901490f5e7f1d3f575d898e9a75487ae
abufaisal179 <abufaishal179@gmail.com>
2025-01-18 22:49:24 +0530

    Update tempCodeRunnerFile.cpp

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,16 +1,16 @@
-// #include <iostream>
-// // Hey guys , its Faisal's Program : TITLE >> {}
-// #include <iostream>
-// using namespace std;
-// void printnumber( int n )
-// {
-//     cout << n << " ";
-
-//    n =  printnumber(n)+1;
-    
-// }
-// int main(void) {
-
-// printnumber(0);
-//   return 0;
-// }
+#include <iostream>
+using namespace std;
+int print()
+{
+    return 1;
+}
+int main (void)
+{
+ int i=34576521;
+   while( true )
+   {
+      cout << i << endl;
+      i++;
+   }
+    return 0;
+}
