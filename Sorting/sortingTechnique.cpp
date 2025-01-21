6a04630776dae445ecca3ac29e67fd315c59c98d
abufaisal179 <abufaishal179@gmail.com>
2025-01-21 22:37:33 +0530

    Create sortingTechnique.cpp

---
+++ Sorting/sortingTechnique.cpp
@@ -0,0 +1,14 @@
+// Hey guys , its Faisal's Program : TITLE >> {}
+#include <iostream>
+using namespace std;
+void printHello(int data);
+int main(void)
+{
+    for( int i=0; i<5; i++ )
+    printHello(i*431-12);
+    return 0;
+}
+void printHello(int data)
+{
+    cout << data << endl;
+}
