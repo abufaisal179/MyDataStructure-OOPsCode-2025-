57a19f8430c2ccd151f16c2b3e3ebae8db7e89df
abufaisal179 <abufaishal179@gmail.com>
2025-01-24 23:34:30 +0530

    Update 2dArrayTraversal.cpp

--- 2D_Arrays/2dArrayTraversal.cpp
+++ 2D_Arrays/2dArrayTraversal.cpp
@@ -2,22 +2,25 @@
 #include <iostream>
 using namespace std;
 
-void printArrayElement ( int arr[][3], int rows , int column )
+void printArrayElement(int arr[][3], int rows, int column)
 {
-    for( int i=0; i<rows; i++ )
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < column; j++)
     {
-        for( int j=0; j<column; j++ )
-        {
-              cout << arr[i][j] << "    ";
-        }
-        cout << endl;
+      cout << arr[i][j] << "    ";
     }
+    cout << endl;
+  }
 }
 
-int main(void) {
+int main(void)
+{
 
-int arr[3][3] = { 2 , 1 , 9 , 4 , 2 , 89 , 12, 45, 23 };
+  int arr[3][3] = {{2, 1, 9},
+                   {4, 2, 89},
+                   {12,45,23}};
 
-printArrayElement( arr , 3 , 3 );
+  printArrayElement(arr, 3, 3);
   return 0;
 }
