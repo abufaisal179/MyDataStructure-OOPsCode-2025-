d3c469e3d612462a0398411d39e25fe9ed407890
abufaisal179 <abufaishal179@gmail.com>
2025-02-01 16:15:08 +0530

    Update reverseDoublyLinkedList.cpp

--- linkedList/reverseDoublyLinkedList.cpp
+++ linkedList/reverseDoublyLinkedList.cpp
@@ -1,5 +1,6 @@
 // Hey guys , its Faisal's Program : TITLE >> {}
 #include <iostream>
+#include <stack>
 using namespace std;
 class Node
 {
@@ -12,45 +13,87 @@ public:
         this->data = data;
         prev = next = NULL;
     }
-  
+    ~Node()
+    {
+        int value = this->data;
+        if (next != NULL)
+        {
+            delete next;
+            next = NULL;
+        }
+        cout << "memeory is free for data " << value << endl;
+    }
 };
+Node *reverseWithLinks( Node *&head )
+{
+    if( head -> next == NULL )
+    return head;
+
+    Node *last = NULL;
+    Node *curr = head;
+    Node *temp;
+    while( curr != NULL )
+    {
+        temp = curr -> prev;
+        curr -> prev = curr -> next;
+        curr -> next = temp;
+        curr = curr -> prev;
+    }
+     head = temp -> prev;
+}
+void reverseNodeWithStack( Node *&head )
+{
+    stack<int>S1;
+    Node *temp = head;
+    while( temp != NULL )
+    {
+        S1.push( temp -> data);
+        temp = temp -> next;
+    }
+     temp = head;
+     while( temp != NULL )
+    {
+        temp -> data = S1.top();
+        temp = temp -> next;
+        S1.pop();
+    }
+}
 void insertAtHead(int data, Node *&head)
 {
+    if (head == NULL)
+    {
+        Node *temp = new Node(data);
+        head = temp;
+        return;
+    }
     Node *temp = new Node(data);
 
     temp->next = head;
     head->prev = temp;
     head = temp;
 }
-void reverseNode( Node *&head )
-{
-    while( head != NULL )
-    {
-        head = head -> next;
-    }
-}
 void print(Node *head)
 {
     while (head != NULL)
     {
         cout << head->data << " ";
-        head = head -> prev;
+        head = head->next;;
     }
-    
+    cout << endl;
 }
 int main(void)
 {
+    Node *head = NULL;
 
-    Node *head = new Node(9);
-    Node *tail = head;
-    insertAtHead(3, head);
+    insertAtHead(5, head);
     insertAtHead(23, head);
-    insertAtHead(2, head);
-    insertAtHead( 5, head );
-
-    reverseNode( head );
+    insertAtHead(9, head);
+    insertAtHead(4, head);
  
-    print( head );
+
+    print(head);
+    reverseWithLinks(head);
+    print(head);
 
     return 0;
 }
