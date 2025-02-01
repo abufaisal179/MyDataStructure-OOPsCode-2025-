ffaa65ca04d9fe13b7f5283094cfcb40ba3adacc
abufaisal179 <abufaishal179@gmail.com>
2025-02-01 16:14:26 +0530

    Update linkedListTraversal.cpp

--- linkedList/linkedListTraversal.cpp
+++ linkedList/linkedListTraversal.cpp
@@ -57,7 +57,23 @@ void insertAtMiddle(int data, Node *&head, Node *&tail, int position)
     nodeToInsert->next = temp->next;
     temp->next = nodeToInsert;
 }
+bool isCircular(Node *&head) {
+    if (head == NULL) 
+        return true;  // An empty list is considered circular
+    if (head->next == head) 
+        return true;  // A single node pointing to itself is circular
+    if( head -> next == NULL )
+    return false;
 
+    Node *temp = head->next;  // Start from the second node
+
+    // Traverse the list and check if we reach back to the head
+    while (temp != NULL && temp != head) {
+        temp = temp->next;
+    }
+
+    return (temp == head);  // If we reach back to the head, the list is circular
+}
 int main(void)
 {
 
@@ -75,6 +91,11 @@ int main(void)
          << "head >> "<< head->data;
     cout << endl 
          << "tail >> "<< tail->data;
+
+    cout << endl;
+    if( isCircular(head) == true ) cout << "the list is circular ";
+    else cout<< "list is not circular ";
+
 
     return 0;
 }
