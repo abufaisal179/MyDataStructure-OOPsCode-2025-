b094b040d993ae15406b7e0a0fd5941df8cf3b11
abufaisal179 <abufaishal179@gmail.com>
2025-02-01 16:13:49 +0530

    Create class2stp.cpp

---
+++ .vscode/VishalCodes/class2stp.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+class fruits {
+    public:
+    string name;
+    int code;
+};
+fruits f1,f2;
+int main(void){
+cout <<"Enter the fruits name  : ";
+cin >>f1.name;
+cout <<"Enter the fruits code : ";
+cin >>f1.code;
+cout <<"Enter the fruits name f2 : ";
+cin >>f2.name;
+cout <<"Enter the fruits code : ";
+cin >>f2.code;
+
+
+    cout << f1.name <<' '<<f1.code<<" "<< endl <<  f2.name << " " << f2.code << endl;
+
+    return 0;
+}
