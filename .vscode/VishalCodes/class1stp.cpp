65c075256004cd8b47ff3e4b033fd63385b4081a
abufaisal179 <abufaishal179@gmail.com>
2025-02-01 16:13:41 +0530

    Create class1stp.cpp

---
+++ .vscode/VishalCodes/class1stp.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+class fruits 
+{
+    private:
+    string name;
+    int code;
+   
+    public:
+    fruits( string name , int code )
+    {
+        this->name = name;
+        this->code = code;
+    }
+    // method
+    void printData()
+    {
+        cout << name << " " << code << endl;
+    } 
+};
+int main(void)
+{
+
+    fruits f1("Mango" , 4);
+
+    f1.printData();
+    
+    return 0;
+}
