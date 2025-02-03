fea501e7a4a3516fc0b532bf80522797db0b3b93
abufaisal179 <abufaishal179@gmail.com>
2025-02-03 19:00:15 +0530

    Create hashingStringO.cpp

---
+++ hashing/hashingStringO.cpp
@@ -0,0 +1,30 @@
+// Hey guys , its Faisal's Program : TITLE >> {}
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    
+
+    string s;
+    cout << " enter string : ";
+    cin >> s;
+
+    int hash[26] = {0};
+    for( int i=0; i< s.size(); i++ )
+    {
+        hash[s[i] - 'a'] += 1;
+    }
+
+ 
+    int q = s.size();
+
+    while( q-- )
+    {
+        char c;
+        cin >> c;
+        cout << hash[c - 'a'] << " ";
+    
+    }
+
+  return 0;
+}
