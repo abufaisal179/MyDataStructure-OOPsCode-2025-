fe7314686a57120fa26da8447f0e3e6cc575485e
abufaisal179 <abufaishal179@gmail.com>
2025-02-05 22:47:48 +0530

    Update fact.cpp

--- .vscode/recursion/fact.cpp
+++ .vscode/recursion/fact.cpp
@@ -1,24 +1,22 @@
 // Hey guys , its Faisal's Program : TITLE >> {}
 #include <iostream>
 using namespace std;
-int factorial ( int number )
+int factorial(int number)
 {
-
-
-    if( number == 1 )
+  if (number == 1)
     return 1;
 
-    return number * factorial( number -1 );
+  return number * factorial(number - 1);
 }
-int main(void) {
+int main(void)
+{
 
-    cout << "enter number : ";
-    int fact ;
-    cin >> fact;
-    int result = factorial(fact);
+  cout << "enter number : ";
+  int fact;
+  cin >> fact;
+  int result = factorial(fact);
 
-
-    cout << result ;
+  cout << result;
 
   return 0;
 }
