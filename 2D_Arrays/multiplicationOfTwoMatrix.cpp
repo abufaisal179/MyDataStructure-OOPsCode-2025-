d79febd165bb8d9058ec7b439da55024adda6c51
abufaisal179 <abufaishal179@gmail.com>
2025-02-11 22:03:10 +0530

    Create multiplicationOfTwoMatrix.cpp

---
+++ 2D_Arrays/multiplicationOfTwoMatrix.cpp
@@ -0,0 +1,37 @@
+// Hey guys , its Faisal's Program : TITLE >> {}
+#include <iostream>
+using namespace std;
+void display( int arr[][3] , int row , int column )
+{
+    for( int i=0; i<row; i++ )
+     {
+        for( int j=0; j<column; j++ )
+        {
+            cout << arr[i][j] << "\t";
+        }
+        cout << endl;
+     }
+
+}
+void multiplication( int arr[][3] , int brr[][3] , int row , int column )
+{
+    int crr[3][3];
+     for( int i=0; i<row; i++ )
+     {
+        for( int j=0; j<column; j++ )
+        {
+            crr[i][j] += arr[i][j] * brr[j][i];
+        }
+     }
+     display( crr , 3 , 3 );
+}
+
+int main(void) {
+
+int arr[3][3] = { 3 , 5 , 12 , 54 , 7 , 98 , 56 , 453, 644 }; 
+int brr[3][3] = { 3 , 5 , 443 , 54 , 9 , 98 , 16 , 43, 64 }; 
+
+multiplication( arr , brr , 3 , 3 );
+
+  return 0;
+}
