6a7d2549c50abaa1fd84ddb56f29eabb683e1781
abufaisal179 <abufaishal179@gmail.com>
2025-02-13 23:17:16 +0530

    Create overloading=operator.cpp

---
+++ oopsProgram/overloading=operator.cpp
@@ -0,0 +1,37 @@
+// Hey guys , its Faisal's Program : TITLE >> {}
+#include <iostream>
+using namespace std;
+class Overload 
+{
+    int a;
+
+
+    public:
+    Overload( int x  )
+    {
+        a = x;
+    }
+    void operator+=( Overload obj2 )
+    {
+        this -> a = obj2.a;
+    }
+    void print()
+    {
+        cout << this->a << " " << endl;
+    }
+};
+
+int main(void) {
+
+    Overload a1( 4 );
+    Overload a2( 2 );
+    a1.print();
+    a2.print();
+
+    cout << "the sum of both is : " << endl;
+    a1 = a2;
+
+    a1.print();
+
+  return 0;
+}
