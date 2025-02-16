ea69c695eeb1aef0fd92ce8767dccc753cdaee18
abufaisal179 <abufaishal179@gmail.com>
2025-02-16 22:55:57 +0530

    Create inlineFunction.cpp

---
+++ oopsProgram/inlineFunction.cpp
@@ -0,0 +1,15 @@
+// Hey guys , its Faisal's Program : TITLE >> {}
+#include <iostream>
+using namespace std;
+inline int add( int a , int b )
+{
+     return a+b;
+}
+int main(void) {
+
+    int x  = 9 , y = 4 ;
+
+    cout << add( x , y );
+
+  return 0;
+}
