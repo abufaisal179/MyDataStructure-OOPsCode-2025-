a659e4e93ed8e7bc06a40cecdd2ebadbabbd2c8c
abufaisal179 <abufaishal179@gmail.com>
2025-02-17 22:38:15 +0530

    Update tempCodeRunnerFile.cpp

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,11 +1,41 @@
+// Hey guys , its Faisal's Program : TITLE >> {}
 #include <iostream>
 using namespace std;
-int main ( void )
+void insertionSort( int *arr , int n )
 {
-    for( int i=0; i<100000000; i++ )
+     for( int i=0; i<n; i++ )
+     {
+        int j=i-1;
+        int temp = arr[i];
+        for( ; j>=0; j-- )
+        {
+            if( temp < arr[j] )
+            {
+                arr[j+1] = arr[j];
+            }
+            else
+            {
+                break;
+            }
+        }
+        arr[j+1] = temp;
+     }
+}
+void display( int *arr , int n )
+{
+    //cout << endl;
+    for( int i=0; i<n; i++ )
     {
-        cout << i << " ";
-        cout << endl;
+        cout << arr[i] << " ";
     }
-    return 0;
+    cout << endl;
+}
+int main(void) {
+    int arr[] = { 121 , 4 , 6 , 2 , 1 , 8 , 5 , 3  };
+
+    int n = sizeof(arr)/sizeof(arr[0]);
+    insertionSort( arr , n );
+    display( arr , n );
+
+  return 0;
 }
