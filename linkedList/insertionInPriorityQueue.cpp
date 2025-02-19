7f4b390d8ff34c5f8a8ace69260911c38dc6fb43
abufaisal179 <abufaishal179@gmail.com>
2025-02-19 20:40:57 +0530

    Update insertionInPriorityQueue.cpp

--- linkedList/insertionInPriorityQueue.cpp
+++ linkedList/insertionInPriorityQueue.cpp
@@ -15,19 +15,7 @@ class Node
           front =  rear = NULL;
     }
 };
-bool IsEmpty( Node *front , Node *rear )
-{
-    if( front == NULL && rear ==  NULL)
-    {
-         return 1;
-    }
-    return 0;
-    // if( IsEmpty(front , rear) == true )
-    // {
-    //     cout << " can't enqueue more priority elements " << endl;
-    //     return;
-    // }
-}
+
 void insertInPriorityQueue( char data , int priority , Node *&front , Node *&rear )
 {
     Node *temp = new Node(data, priority); 
@@ -37,25 +25,36 @@ void insertInPriorityQueue( char data , int priority , Node *&front , Node *&rea
         return;
     }
     Node *head = front;
-    Node *curr = NULL;
-    // managing case where temp -> pri < head -> pri >>
-    if( temp -> priority < head -> priority )
+    Node *curr = front;
+    // for first >>
+    if( temp -> priority <= curr -> priority )
     {
-        temp -> next = head;
-        temp = head;
-    }
-    while( temp -> priority > head -> priority && head != NULL )
-    {
-         curr = head;
-         head = head -> next;
-    }
-    if( head == NULL )
-    {
-        head -> next = temp;
+        temp -> next = curr;
+        curr = temp;
+        front = curr;
         return;
     }
-    curr -> next = temp;
-    temp -> next = head;
+    // for middle >> 
+    while( curr -> next != NULL )
+    {
+        if( temp -> priority <= curr -> next -> priority && temp -> priority >= curr -> priority)
+        {
+            temp -> next = curr -> next;
+            curr -> next = temp;
+            return;
+        }
+        curr = curr -> next;
+    }
+    // for last >>
+    if( temp -> priority >= curr -> priority )
+    {
+        curr -> next = temp;
+        curr = temp;
+        rear = temp;
+    }
+
+    return;
+
 }
 void printAllData(Node *&head)
 {
@@ -71,10 +70,10 @@ int main(void) {
     Node *front = NULL;
     Node *rear = NULL;
 
-    insertInPriorityQueue( 'A' , 1 , front , rear );
-    insertInPriorityQueue( 'F' , 3 , front , rear );
     insertInPriorityQueue( 'n' , 2 , front , rear );
+    insertInPriorityQueue( 'A' , 1 , front , rear );
     insertInPriorityQueue( 'W' , 4 , front , rear );
+    insertInPriorityQueue( 'F' , 3 , front , rear );
 
 
     printAllData( front );
