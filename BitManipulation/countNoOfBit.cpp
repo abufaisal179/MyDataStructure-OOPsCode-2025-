6771a4c63ab6f6bed3f928dba040e3cc4bf80870
abufaisal179 <abufaishal179@gmail.com>
2025-02-23 20:08:38 +0530

    Update countNoOfBit.cpp

--- BitManipulation/countNoOfBit.cpp
+++ BitManipulation/countNoOfBit.cpp
@@ -1,3 +1,25 @@
+// // Hey guys , its Faisal's Program : TITLE >> {}
+// #include <iostream>
+// using namespace std;
+// void clear ( int number , int i )
+// {   
+//     int count = 0;
+//     while( number != 0 )
+//     {
+//           count += ( number & 1); 
+//           number = number >> 1;
+//     }
+//     cout << count << endl;
+// }
+// int main(void) {
+
+//      int i = 2;
+//      for( int i=0; i<50; i++ )
+//     clear ( i, 2 );
+
+//   return 0;
+// }
+
 // Hey guys , its Faisal's Program : TITLE >> {}
 #include <iostream>
 using namespace std;
@@ -6,17 +28,18 @@ void clear ( int number , int i )
     int count = 0;
     while( number != 0 )
     {
-          count += ( number & 1); 
-          number = number >> 1;
+        cout << number << " ";
+        number = (number & number-1);
+        count++;
     }
     cout << count << endl;
 }
 int main(void) {
 
      int i = 2;
-     for( int i=0; i<50; i++ )
-    clear ( i, 2 );
+    clear ( 85, 2 );
 
   return 0;
 }
+ 
  
