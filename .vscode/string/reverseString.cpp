4e76fe70e7f8c491537ed87119cc62b90a7c5902
abufaisal179 <abufaishal179@gmail.com>
2025-02-27 23:06:16 +0530

    Create reverseString.cpp

---
+++ .vscode/string/reverseString.cpp
@@ -0,0 +1,29 @@
+// Hey guys , its Faisal's Program : TITLE >> {}
+#include <iostream>
+using namespace std;
+void reverseString( string &str , int n )
+{
+      int left = 0;
+      int right = n-1;
+
+      while( left < right )
+      {
+        swap( str[left] , str[right] );
+          left++;
+          right--;
+      }
+}
+void display(string str)
+{
+    cout << str << endl;
+}
+int main(void) {
+
+    string str = "Faisal";
+    int size = str.length();
+
+    reverseString( str , size );
+    display( str );
+
+  return 0;
+}
