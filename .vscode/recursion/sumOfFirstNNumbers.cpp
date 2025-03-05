8cc30df1937a33a0bc94a9f6c246cc7276b3c341
abufaisal179 <abufaishal179@gmail.com>
2025-03-05 22:14:38 +0530

    Update sumOfFirstNNumbers.cpp

--- .vscode/recursion/sumOfFirstNNumbers.cpp
+++ .vscode/recursion/sumOfFirstNNumbers.cpp
@@ -1,4 +1,5 @@
 // Hey guys , its Faisal's Program : TITLE >> {}
+// parameterized recuesion  
 #include <iostream>
 using namespace std;
 int returnSum( int i , int sum )
@@ -8,10 +9,19 @@ int returnSum( int i , int sum )
 
    returnSum( i-1 , sum + i );
 }
+// function recusrsion
+int returnSum1( int number )
+{
+    if( number < 1 )
+    return 0;
+
+    return number + returnSum1( number - 1 );
+}
 int main(void) {
 
 
-    cout << returnSum( 5 , 0 );
+    cout << "the sum with function 1 : " << returnSum( 5 , 0 ) << endl;
+    cout << "the sum with function 2 : " << returnSum1( 87 );
 
   return 0;
 }
