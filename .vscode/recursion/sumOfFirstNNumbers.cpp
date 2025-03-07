f875f6534205be15383618d55ac1b320c64b10d5
abufaisal179 <abufaishal179@gmail.com>
2025-03-07 22:37:26 +0530

    Update sumOfFirstNNumbers.cpp

--- .vscode/recursion/sumOfFirstNNumbers.cpp
+++ .vscode/recursion/sumOfFirstNNumbers.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int returnSum( int i , int sum )
 {
    if( i < 1 )
-   return sum;
+   return sum; 
 
    returnSum( i-1 , sum + i );
 }
@@ -17,11 +17,23 @@ int returnSum1( int number )
 
     return number + returnSum1( number - 1 );
 }
+int returnFctorial1( int number )
+{
+     if( number == 1 )
+     {
+        return 1;
+     }
+
+     return number * returnFctorial1( number - 1 );
+}
 int main(void) {
 
 
+  int number;
+  cout << "enter number for factorial and for the summation  : "; cin >> number ;
     cout << "the sum with function 1 : " << returnSum( 5 , 0 ) << endl;
-    cout << "the sum with function 2 : " << returnSum1( 87 );
+    cout << "the sum with function 2 : " << returnSum1( 87 ) << endl;
+    cout << " the factorial of " << number << " is : " << returnFctorial1( number ) << endl;
 
   return 0;
 }
