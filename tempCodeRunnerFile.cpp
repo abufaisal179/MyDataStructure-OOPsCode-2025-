de7292ae11c038121a3ef09cd6ebca3fab475069
abufaisal179 <abufaishal179@gmail.com>
2025-03-13 22:44:00 +0530

    Update tempCodeRunnerFile.cpp

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,41 +1,20 @@
-// Hey guys , its Faisal's Program : TITLE >> {insertion sort}
 #include <iostream>
 using namespace std;
-void insertionSort( int *arr , int n )
+int main ( void )
 {
-     for( int i=0; i<n; i++ )
+ 
+     int value;
+     int f;
+     cout << "enter the number : ";
+     cin >> f;
+     int x=0;
+     while(  f-- )
      {
-        int j=i-1;
-        int temp = arr[i];
-        for( ; j>=0; j-- )
-        {
-            if( temp < arr[j] )
-            {
-                arr[j+1] = arr[j];
-            }
-            else
-            {
-                break;
-            }
-        }
-        arr[j+1] = temp;
-     }
-}
-void display( int *arr , int n )
-{
-    //cout << endl;
-    for( int i=0; i<n; i++ )
-    {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-}
-int main(void) {
-    int arr[] = { 121 , 4 , 6 , 2 , 1 , 8 , 5 , 3  };
+         cout << "enter the number : ";
+        cin >> value;
+    x += value;
 
-    int n = sizeof(arr)/sizeof(arr[0]);
-    insertionSort( arr , n );
-    display( arr , n );
-
-  return 0;
+     } 
+     cout <<"the sum is : " << x;
+    return 0;
 }
