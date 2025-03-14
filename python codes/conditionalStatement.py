1cde80ca93d0a944a8b00900ef54c9a586bf930d
abufaisal179 <abufaishal179@gmail.com>
2025-03-14 17:07:31 +0530

    Update conditionalStatement.py

--- python codes/conditionalStatement.py
+++ python codes/conditionalStatement.py
@@ -1,11 +1,12 @@
 print("enter the marks of All subjects !! " , " and the minimum cutoff for all 3 subs is 99 marks !! ")
-math = input("enter the value for math >> ")
-phy = input("enter the value for physics >> ")
-che = input("enter the value for chemistry >> ")
+math = int(input("enter the value for math >> "))
+phy = int(input("enter the value for physics >> "))
+che = int(input("enter the value for chemistry >> "))
 
 sumOfvalues = phy + che + math
-
-if  99 <= sumOfvalues <= 150 :
+if 0 <= sumOfvalues <= 98 :
+    print("you got failed in your examination , because your marks is <= 98 ") 
+elif  99 <= sumOfvalues <= 150 :
     print("you passed with 3rd division !! ")
     print("and your marks is b/w 99-150 ")
 elif 150 <= sumOfvalues <=200 :
