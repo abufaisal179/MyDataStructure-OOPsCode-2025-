0c5ea009ba6c904817ed60f7067383e285c8ef0f
abufaisal179 <abufaishal179@gmail.com>
2025-03-19 23:03:31 +0530

    Create appendListByUser.py

---
+++ python codes/appendListByUser.py
@@ -0,0 +1,6 @@
+list = []
+list.append( input("enter the name of 1st movie : " ))
+list.append( input("enter the name of 2nd movie : " ))
+list.append( input("enter the name of 3rd movie : " ))
+
+print(list)
