6871cd1ad5901ef2f31875040caad30551333040
abufaisal179 <abufaishal179@gmail.com>
2025-03-20 22:47:05 +0530

    Create plaindromeList.py

---
+++ python codes/plaindromeList.py
@@ -0,0 +1,11 @@
+list = [ 1 ,5 ,5 , 2 ,1 ]
+list2 = list
+print(list2 , " " , list)
+list2.reverse()
+print( list2 )
+if list == list2 :
+    print("plaindrome list ")
+else :
+    print(" not a plaindrone list ")
+
+print( "end of programme ")
