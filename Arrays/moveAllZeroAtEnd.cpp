a45506b10b6fecef8d0f35400b4e18b9273e75c2
abufaisal179 <abufaishal179@gmail.com>
2025-03-23 20:41:41 +0530

    Update moveAllZeroAtEnd.cpp

---
+++ Arrays/moveAllZeroAtEnd.cpp
@@ -0,0 +1,42 @@
+// Hey guys , its Faisal's Program : TITLE >> {}
+#include <iostream>
+using namespace std;
+void moveZero( int *arr , int n )
+{
+    int j=0;
+    for( int i=0; i<n; i++ )
+    {
+        if( arr[i] == 0 )
+        {
+            j = i;
+            break;
+        }
+    }
+
+    for( int i=j+1; i<n; i++ )
+    {
+        if( arr[i] != 0 )
+        {
+            swap( arr[i] , arr[j] );
+            j++;
+        }
+
+    }
+}
+void print( int *arr , int n , int d )
+{
+    for( int i=d; i<n; i++ )
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+int main(void) {
+
+    int arr[7] = { 3 , 6 , 0 , 5 , 12 , 0 , 34 };
+
+    moveZero( arr , 7 );
+    print( arr , 7 , 0 );
+
+  return 0;
+}
