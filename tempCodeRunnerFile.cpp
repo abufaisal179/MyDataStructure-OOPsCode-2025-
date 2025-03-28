026ce37d6eafa78c8b373685807d40d703dd35f6
abufaisal179 <abufaishal179@gmail.com>
2025-03-28 23:26:45 +0530

    Update tempCodeRunnerFile.cpp

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -2,19 +2,15 @@
 using namespace std;
 int main ( void )
 {
- 
-     int value;
-     int f;
-     cout << "enter the number : ";
-     cin >> f;
-     int x=0;
-     while(  f-- )
-     {
-         cout << "enter the number : ";
-        cin >> value;
-    x += value;
 
-     } 
-     cout <<"the sum is : " << x;
+    int store = 1;
+    int fact  = 5;
+    for( int i=1; i<=fact; i++ )
+    {
+        store = store * i;
+    }
+
+    cout << store ;
+
     return 0;
 }
