0b88ad3f0ac3a3266f6c31309539aacf56e1fd8e
abufaisal179 <abufaishal179@gmail.com>
2025-03-31 11:51:16 +0530

    Update findingAverageUsingFriendFunc.cpp

--- oopsProgram/findingAverageUsingFriendFunc.cpp
+++ oopsProgram/findingAverageUsingFriendFunc.cpp
@@ -1,11 +1,30 @@
 // Hey guys , its Faisal's Program : TITLE >> {}
 #include <iostream>
 using namespace std;
-class Average
+#define AVG 3
+class Averages
 {
     private:
-}
+    int number1 , number2 , number3;
+    public:
+    Averages( int number1 , int number2 , int number3 )
+    {
+        this-> number1 = number1;
+        this-> number2 = number2;
+        this-> number3 = number3;
+    }
+
+    void display(void)
+    {
+        cout << ( number1 + number2 + number3 ) / 3 << endl;
+    }
+
+};
 int main(void) {
+
+
+  Averages avg1( 90 , 76 , 87 );
+  avg1.display();
 
   return 0;
 }
