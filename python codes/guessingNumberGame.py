7f7fbb89fcc8fcfa0306a56a9f79cc13403de84d
abufaisal179 <abufaishal179@gmail.com>
2025-04-02 23:31:33 +0530

    Create guessingNumberGame.py

---
+++ python codes/guessingNumberGame.py
@@ -0,0 +1,13 @@
+target = 56
+
+for it in range(1 , 100) :
+    number = int(input("enter the number : "))
+    if( number == target ) :
+        print("congratulations you found the number !!..")
+        break
+    else :
+        print("you enterd wrong number.. Try again..")
+
+print("END OF CODE ")
+
+
