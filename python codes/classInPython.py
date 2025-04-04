d8d237bc1dd9d6ab24053e308796458459b011cb
abufaisal179 <abufaishal179@gmail.com>
2025-04-04 16:12:06 +0530

    Create classInPython.py

---
+++ python codes/classInPython.py
@@ -0,0 +1,8 @@
+class Student :
+    my_college = "SHEAT Varanasi"
+    def __init__(self , full_name ) : 
+        self.name = full_name
+    def printName(self) :
+        print("the name of student is : " , self.name )
+s1 = Student("faisal")
+s1.printName()
