75e51098d3183fbd02ae21fcdce7f441b7bf0d7d
abufaisal179 <abufaishal179@gmail.com>
2025-04-05 16:04:44 +0530

    Update tempCodeRunnerFile.cpp

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,26 +1,39 @@
 #include <iostream>
 // Hey guys , its Faisal's Program : TITLE >> {}
+#include <iostream>
 using namespace std;
-void print( int n )
+void delElement( int *arr , int target , int *n )
 {
-    int number1 = 0 , number2 = 1;
-    printf(" 0 1 ");
-
-    n-=2;
-    while( n-- )
+     for( int i=0; i<*n; i++ )
+     {
+        if( arr[i] == target )
+        {
+            for( int j=i; j<*n; j++ )
+            {
+                arr[j] = arr[j+1];
+            }
+        }
+     }
+     n--;
+}
+void display(int *arr , int n )
+{
+    for( int i=0; i<n; i++ )
     {
-        int next = number1 + number2;
-        cout << next << " ";
-        number1 = number2;
-        number2 = next;
+        cout << arr[i] << " ";
     }
+
+    cout << endl;
 }
 int main(void) {
 
+    int arr[5] = {5 , 7 , 8 ,2 , 1 };
 
-    print(5);
+    int n = 5;
 
+    delElement( arr , 7 , &n );
+
+    display( arr , 5 );
 
   return 0;
 }
-
