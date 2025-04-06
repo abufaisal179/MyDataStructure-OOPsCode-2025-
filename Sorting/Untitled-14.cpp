59afa3d17a0b2ad55122313188c02a3b01b00769
abufaisal179 <abufaishal179@gmail.com>
2025-04-06 14:25:01 +0530

    Create Untitled-14.cpp

---
+++ Sorting/Untitled-14.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+// Hey guys , its Faisal's Program : TITLE >> {}
+#include <iostream>
+using namespace std;
+void delElement( int *arr , int target , int *n )
+{
+     for( int i=0; i<*n; i++ )
+     {
+        if( arr[i] == target )
+        {
+            for( int j=i; j<*n; j++ )
+            {
+                arr[j] = arr[j+1];
+            }
+        }
+     }
+     n--;
+}
+void display(int *arr , int n )
+{
+    for( int i=0; i<n; i++ )
+    {
+        cout << arr[i] << " ";
+    }
+
+
+    
+
+    cout << endl;
+}
+int main(void) {
+
+    int arr[5] = {5 , 7 , 8 ,2 , 1 };
+
+    int n = 5;
+
+    delElement( arr , 7 , &n );
+
+    display( arr , 5 );
+
+  return 0;
+}
