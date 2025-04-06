2d4e669bdfb4e2a3386b82c707db8e943b387e91
abufaisal179 <abufaishal179@gmail.com>
2025-04-06 14:25:23 +0530

    Create Untitled-5.cpp

---
+++ Sorting/Untitled-5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+// Hey guys , its Faisal's Program : TITLE >> {}
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  
+  int temp = 19;
+  temp = temp % 10;
+  cout << temp;
+  return 0;
+}
