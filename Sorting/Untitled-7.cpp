0b4555c0831c3e24d67244067828ea565df52ea5
abufaisal179 <abufaishal179@gmail.com>
2025-04-06 14:25:31 +0530

    Create Untitled-7.cpp

---
+++ Sorting/Untitled-7.cpp
@@ -0,0 +1,41 @@
+// Hey guys , its Faisal's Program : TITLE >> {insertion sort}
+#include <iostream>
+using namespace std;
+void insertionSort( int *arr , int n )
+{
+     for( int i=0; i<n; i++ )
+     {
+        int j=i-1;
+        int temp = arr[i];
+        for( ; j>=0; j-- )
+        {
+            if( temp < arr[j] )
+            {
+                arr[j+1] = arr[j];
+            }
+            else
+            {
+                break;
+            }
+        }
+        arr[j+1] = temp;
+     }
+}
+void display( int *arr , int n )
+{
+    //cout << endl;
+    for( int i=0; i<n; i++ )
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+int main(void) {
+    int arr[] = { 121 , 4 , 6 , 2 , 1 , 8 , 5 , 3  };
+
+    int n = sizeof(arr)/sizeof(arr[0]);
+    insertionSort( arr , n );
+    display( arr , n );
+
+  return 0;
+}
