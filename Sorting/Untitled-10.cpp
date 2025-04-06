76e7cfecb35541927c248c3702ced31ba0dce73e
abufaisal179 <abufaishal179@gmail.com>
2025-04-06 14:24:44 +0530

    Create Untitled-10.cpp

---
+++ Sorting/Untitled-10.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+// Hey guys , its Faisal's Program : TITLE >> {}
+#include <iostream>
+using namespace std;
+
+int main(void) {
+   
+
+    printf("%d" , 13&4 );
+  return 0;
+}
