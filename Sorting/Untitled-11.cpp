e7208538acac3ae2c20cc1be9b9417c9b4657c9c
abufaisal179 <abufaishal179@gmail.com>
2025-04-06 14:24:49 +0530

    Create Untitled-11.cpp

---
+++ Sorting/Untitled-11.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main ( void )
+{
+ 
+     int value;
+     int f;
+     cout << "enter the number : ";
+     cin >> f;
+     int x=0;
+     while(  f-- 
+    )
+     {
+         cout << "enter the number : ";
+        cin >> value;
+    x += value;
+
+     } 
+     cout <<"the sum is : " << x;
+    return 0;
+}
