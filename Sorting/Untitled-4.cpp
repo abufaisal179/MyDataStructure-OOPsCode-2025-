00fabe5431ad124e2bfad6a2005466e961427051
abufaisal179 <abufaishal179@gmail.com>
2025-04-06 14:25:18 +0530

    Create Untitled-4.cpp

---
+++ Sorting/Untitled-4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <limits.h>
+using namespace std;
+int main (void)
+{
+    for( int i=1; i < 876543; i++ )
+    {
+        std::cout << i << endl;
+    }
+    return 0;
+}
