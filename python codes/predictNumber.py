382d736bc5d85417440ec62be8174a9b8e2d2984
abufaisal179 <abufaishal179@gmail.com>
2025-04-09 00:46:05 +0530

    Update predictNumber.py

--- python codes/predictNumber.py
+++ python codes/predictNumber.py
@@ -4,12 +4,13 @@ for i in range( 1 , 100 ) :
     number = int(input("enter number : "))
     if number == target:
         print("you find the number , target matched ...")
+        break
     else:
-        if number > target:
-            print("you're entering larger number than target...")
-        elif (number < (target+10)) or (number > (target-10) ):
+        if number > (target-10) and number < (target+10):
             print("you're too closed to your number...")
-        else:
+        elif number > target:
+            print("you're entering larger number than target...")
+        else:  
             print("you're entering smaller number than target number...")
 
 print ("END OF CODE ")
