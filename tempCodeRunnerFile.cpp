6631d2b845b6642f3da57b4fc34e825a21aaad97
abufaisal179 <abufaishal179@gmail.com>
2025-04-08 14:15:14 +0530

    Update tempCodeRunnerFile.cpp

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,19 +1,80 @@
 #include <iostream>
-#include <string.h>
+#include <conio.h>     // For _kbhit() and _getch()
+#include <windows.h>   // For Sleep() and system("cls")
+#include <cstdlib>     // For rand()
+#include <ctime>       // For time()
+
 using namespace std;
-void conString( string str1 , string str2 )
-{
-    strcat( str1 , str2 );
 
+const int WIDTH = 40;
+const int HEIGHT = 20;
+int score = 0;
+
+int ballX, ballY;
+int basketX = WIDTH / 2 - 3;
+const int basketWidth = 7;
+
+void setup() {
+    srand(time(0));
+    ballX = rand() % WIDTH;
+    ballY = 0;
 }
-int main ( void )
-{
 
-    string str1 = " faisal ";
-    string str2 = " abu ";
+void draw() {
+    system("cls");  // Clear console
 
-    conString( str1 , str2 );
+    for (int i = 0; i < WIDTH + 2; i++) cout << "#";
+    cout << "\n";
 
-    cout << str1 << endl;
+    for (int y = 0; y < HEIGHT; y++) {
+        for (int x = 0; x < WIDTH; x++) {
+            if (x == 0) cout << "#";
+
+            if (x == ballX && y == ballY)
+                cout << "O"; // Ball
+            else if (y == HEIGHT - 1 && x >= basketX && x < basketX + basketWidth)
+                cout << "="; // Basket
+            else
+                cout << " ";
+
+            if (x == WIDTH - 1) cout << "#";
+        }
+        cout << "\n";
+    }
+
+    for (int i = 0; i < WIDTH + 2; i++) cout << "#";
+    cout << "\nScore: " << score << "\n";
+}
+
+void input() {
+    if (_kbhit()) {
+        char ch = _getch();
+        if (ch == 'a' && basketX > 0)
+            basketX--;
+        else if (ch == 'd' && basketX < WIDTH - basketWidth)
+            basketX++;
+    }
+}
+
+void logic() {
+    ballY++;
+
+    if (ballY == HEIGHT - 1) {
+        if (ballX >= basketX && ballX < basketX + basketWidth) {
+            score++;
+        }
+        ballY = 0;
+        ballX = rand() % WIDTH;
+    }
+}
+
+int main() {
+    setup();
+    while (true) {
+        draw();
+        input();
+        logic();
+        Sleep(100);  // Delay
+    }
     return 0;
 }
