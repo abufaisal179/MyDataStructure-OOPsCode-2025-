9a332c937446d224d679ae31fb866ea2a15764b6
abufaisal179 <abufaishal179@gmail.com>
2025-04-14 21:26:58 +0530

    Update tempCodeRunnerFile.cpp

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,80 +1,33 @@
 #include <iostream>
-#include <conio.h>     // For _kbhit() and _getch()
-#include <windows.h>   // For Sleep() and system("cls")
-#include <cstdlib>     // For rand()
-#include <ctime>       // For time()
-
 using namespace std;
-
-const int WIDTH = 40;
-const int HEIGHT = 20;
-int score = 0;
-
-int ballX, ballY;
-int basketX = WIDTH / 2 - 3;
-const int basketWidth = 7;
-
-void setup() {
-    srand(time(0));
-    ballX = rand() % WIDTH;
-    ballY = 0;
-}
-
-void draw() {
-    system("cls");  // Clear console
-
-    for (int i = 0; i < WIDTH + 2; i++) cout << "#";
-    cout << "\n";
-
-    for (int y = 0; y < HEIGHT; y++) {
-        for (int x = 0; x < WIDTH; x++) {
-            if (x == 0) cout << "#";
-
-            if (x == ballX && y == ballY)
-                cout << "O"; // Ball
-            else if (y == HEIGHT - 1 && x >= basketX && x < basketX + basketWidth)
-                cout << "="; // Basket
-            else
-                cout << " ";
-
-            if (x == WIDTH - 1) cout << "#";
-        }
-        cout << "\n";
-    }
-
-    for (int i = 0; i < WIDTH + 2; i++) cout << "#";
-    cout << "\nScore: " << score << "\n";
-}
-
-void input() {
-    if (_kbhit()) {
-        char ch = _getch();
-        if (ch == 'a' && basketX > 0)
-            basketX--;
-        else if (ch == 'd' && basketX < WIDTH - basketWidth)
-            basketX++;
+string returnFav( int number )
+{
+    switch(number)
+    {
+        case 1:
+        return "milk";
+        case 2:
+        return "coke";
+        case 3:
+        return "water";
+        case 4:
+        return "sprite";
+        case 5:
+        return "juice";
+        default:
+        return "error";
     }
 }
+int main ( void )
+{
 
-void logic() {
-    ballY++;
 
-    if (ballY == HEIGHT - 1) {
-        if (ballX >= basketX && ballX < basketX + basketWidth) {
-            score++;
-        }
-        ballY = 0;
-        ballX = rand() % WIDTH;
-    }
-}
+    cout << "enter number : ";
+    int number;
 
-int main() {
-    setup();
-    while (true) {
-        draw();
-        input();
-        logic();
-        Sleep(100);  // Delay
-    }
+    cin >> number;
+
+    cout << returnFav( number );
+
     return 0;
 }
