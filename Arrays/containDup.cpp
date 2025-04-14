0cead38e2ceeead514225fcefe0579d28a9609f6
abufaisal179 <abufaishal179@gmail.com>
2025-04-14 21:26:55 +0530

    Create containDup.cpp

---
+++ Arrays/containDup.cpp
@@ -0,0 +1,23 @@
+// Hey guys , its Faisal's Program : TITLE >> {}
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+int returnIdx( int *arr , int n )
+{
+       unordered_map<int , int > index;
+
+       int i=0;
+       while( i < n )
+       {
+            index[arr[i]]++;
+       }
+
+       for( auto it :  )
+       {
+          
+       }
+}
+int main(void) {
+
+  return 0;
+}
