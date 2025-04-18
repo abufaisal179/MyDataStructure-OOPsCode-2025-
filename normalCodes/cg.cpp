eb79e7e5b41768f14c6a1eb6c257dcab3c70fc62
abufaisal179 <abufaishal179@gmail.com>
2025-04-18 23:10:58 +0530

    Create cg.cpp

---
+++ normalCodes/cg.cpp
@@ -0,0 +1,19 @@
+// Hey guys , its Faisal's Program : TITLE >> {}
+#include <iostream>
+#include <conio.h>
+using namespace std;
+void main(void) {
+
+    int i , j , k;
+
+    clrscr();
+
+    for( int i=0; i<50; i++ )
+    {
+        gotoxy( 10 + i , 20 );
+
+        
+    }
+
+  getch();
+}
