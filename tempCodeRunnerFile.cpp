0e2eff52f80f2f9daaaf60721f07bc5a11af66ac
abufaisal179 <abufaishal179@gmail.com>
2025-04-21 11:12:09 +0530

    Update tempCodeRunnerFile.cpp

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,54 +1,26 @@
 #include <iostream>
 using namespace std;
-void printArray( int *arr , int  n )
+class Demo
 {
+    public:
+    static string str;
 
-    for( int i=0; i<n; i++ )
-    printf("%d " , arr[i]);
-
-    cout << endl;
-}
-void selectionSort( int *arr , int n )
-{
-    printArray( arr , n );
-   if( n == 1 )
-   {
-      cout << " invalid array ";
-      return;
-   }
-   
-   else 
-   {
-      
-      int i = 0;
-      int minIdx;
-      while( i < n - 1 )
-      {
-        minIdx = i;
-        int j=i+1;
-         while( j < n )
-         {
-            if( arr[j] < arr[minIdx] )
-            swap( arr[j] , arr[minIdx] );
- 
-            j++;
-         }
-        printArray( arr , n );
-          i++;
-      }
-
-
-   }
-}
+    void putName( string name )
+    {
+        str = name;
+    }
+    void printName(void)
+    {
+        cout << "the name for this obj is : " << str << endl;
+    }
+};
+string Demo::str;
 int main ( void )
 {
+   Demo d1 , d2;
+   d1.putName("faisal");
+   d1.printName();
+   d2.printName();
 
-    int arr[] = { 4 , 6 , 2 , 3  , 5 , 2 , 1 , 7 };
-
-    int n = sizeof(arr)/sizeof(int);
-
-    selectionSort( arr  , n );
-
-    printArray( arr , n );
     return 0;
 }
