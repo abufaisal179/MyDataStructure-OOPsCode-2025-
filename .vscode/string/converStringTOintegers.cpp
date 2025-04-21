3870d108af3b1e88a629f893fd6d9433abb049b5
abufaisal179 <abufaishal179@gmail.com>
2025-04-21 23:06:23 +0530

    Create converStringTOintegers.cpp

---
+++ .vscode/string/converStringTOintegers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int stringConversion(const string str) {
+    if (str.empty()) return -1;
+
+    int container = 0;
+
+    for (char ch : str) {
+        if (!isdigit(ch)) return -1; // Early return on non-digit
+        container = container * 10 + (ch - '0');
+    }
+
+    return container;
+}
+
+int main() {
+    string str = "12345";
+
+    cout << "The integer is: " << stringConversion(str);
+
+    return 0;
+}
