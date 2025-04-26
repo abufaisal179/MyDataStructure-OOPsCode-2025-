777ef092d8c81c8213cbcfdaf14ab24ea7393b66
abufaisal179 <abufaishal179@gmail.com>
2025-04-26 22:23:24 +0530

    Create useHashMap.cpp

---
+++ Arrays/useHashMap.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+void printMap( int *arr , int n )
+{
+    unordered_map< int , int > map;
+
+    for( int i=0; i<n; i++ )
+    {
+        map.
+    }
+
+
+}
+int main ( void )
+{
+
+    int arr[] = { 5 , 3 , 6 , 1 , 9 , 54 , 34 , 45 };
+    int size = sizeof(arr)/sizeof(int);
+
+
+    printMap( arr , size );
+
+}
