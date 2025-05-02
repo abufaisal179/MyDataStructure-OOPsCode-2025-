5126dfc4092086f830a3ea70bad3ae5be4911e1a
abufaisal179 <abufaishal179@gmail.com>
2025-05-02 22:53:11 +0530

    Create myExp.cpp

---
+++ normalCodes/myExp.cpp
@@ -0,0 +1,16 @@
+// Hey guys , its Faisal's Program : TITLE >> {}
+#include <iostream>
+using namespace std;
+
+int main(void) {
+
+
+    int a = 10;
+    float b = 4.5;
+
+    float c = a+b;
+    cout << "the sum is : " << a+b << endl;
+    cout << "the sum is : " << c;
+
+  return 0;
+}
