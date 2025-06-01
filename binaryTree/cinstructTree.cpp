b7d46f841f31533dbe4f06b4fc081e7505d0efc3
abufaisal179 <abufaishal179@gmail.com>
2025-06-01 16:16:37 +0530

    Create cinstructTree.cpp

---
+++ binaryTree/cinstructTree.cpp
@@ -0,0 +1,20 @@
+// Hey guys , its Faisal's Program : TITLE >> {}
+#include <iostream>
+using namespace std;
+class Node 
+{
+    int data;
+    Node* left;
+    Node* right;
+    
+    
+    Node ( int data )
+    {
+        this -> data = data;
+        left = right = NULL;
+    }
+};
+int main(void) {
+
+  return 0;
+}
