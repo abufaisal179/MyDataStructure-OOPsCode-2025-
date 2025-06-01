969bad70b7183d35303ec5c5cbd5dc4e72e58554
abufaisal179 <abufaishal179@gmail.com>
2025-06-01 16:17:57 +0530

    Update tempCodeRunnerFile.cpp

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,26 +1,12 @@
 #include <iostream>
 using namespace std;
-class Demo
-{
-    public:
-    static string str;
-
-    void putName( string name )
-    {
-        str = name;
-    }
-    void printName(void)
-    {
-        cout << "the name for this obj is : " << str << endl;
-    }
-};
-string Demo::str;
 int main ( void )
 {
-   Demo d1 , d2;
-   d1.putName("faisal");
-   d1.printName();
-   d2.printName();
+
+    for( int i=0; i<10000; i++ )
+    {
+        int j = i;
+    }
 
     return 0;
 }
